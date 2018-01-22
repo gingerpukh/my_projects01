d9ea13eb0c5951ab3fe2cab636dde38ac7f8aa6d
gingerpukh <32568505+gingerpukh@users.noreply.github.com>
2018-01-22 15:39:05 +0530

    first commit

---
+++ src/profiles_project/profiles_api/urls.py
@@ -0,0 +1,19 @@
+# Django viewset
+from django.conf.urls import include
+from rest_framework.routers import DefaultRouter
+
+from django.conf.urls import url
+
+from . import views
+
+# registriraj router
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet) # ne treba base_name jer Django rest_framework skuzi
+router.register('login', views.LoginViewSet, base_name='login')
+router.register('feed', views.UserProfileFeedViewSet)
+
+urlpatterns = [
+    url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'', include(router.urls)) # django router
+]
